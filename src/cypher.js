3d21c5daeda25a26ac12f272d64364b3c5c4c019
Lucca-Urso <lgursog13@gmail.com>
2025-10-30 20:24:06 -0300

    cypher test

---
+++ src/cypher.js
@@ -0,0 +1,46 @@
+// Atbash Cypher
+function atBashCypher(input) {
+    var codedInput = "";
+
+    for (i = 0; i < input.length; i++) {
+        var ascii = input.charCodeAt(i);
+
+        if (ascii < 90) {
+            ascii = 90 - ascii + 65;
+        }
+        else {
+            ascii = 122 - ascii + 97;
+        }
+
+        codedInput += String.fromCharCode(ascii);
+    }
+
+    return codedInput;
+}
+
+// Cesar Cypher
+function cesarCypher(input, displacement) {
+    var codedInput = ""
+
+    for (i = 0; i < input.length; i++) {
+        var ascii = input.charCodeAt(i) + displacement;
+
+        if (ascii > 90)
+            ascii = ascii
+    }
+
+}
+
+// Vigenère Cypher
+
+
+// RSA Cypher
+
+
+
+function main() {
+    console.log(`AtBash Cypher for the input string (OlaMundo) -> ${atBashCypher("OlaMundo")}`);
+    console.log(`Cesar Cypher for the input string (fulswrjudiia) with coeficient with value (-3) -> ${cesarCypher("fulswrjudiia", -3)}`)
+}
+
+main();
